335bac6313393b743aa8279c3c04e8370d33ef27
Carlos H. Marian <carlosmarian@gmail.com>
2023-02-23 21:24:36 -0300

    Ajustes em layout. Layout refactory

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'Editar usuário')
 @section('content')
-<h1>Editar Usuário</h1>
+<h1 class="text-2xl font-semibold leading-tigh py-2">Editar o Usuário {{ $user->name }}</h1>
 
 @include('includes.validation-form')
 
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Usuário {{ $user->name }}')
 
 @section('content')
-<h1>Listagem do usuário {{ $user->name }}</h1>
+<h1 class="text-2xl font-semibold leading-tigh py-2">Listagem do usuário {{ $user->name }}</h1
 <ul>
     <li>{{ $user->name }}</li>
     <li>{{ $user->email }}</li>
@@ -12,7 +12,7 @@
 <form action="{{ route('users.delete', $user->id) }}" method="post">
     @method('DELETE')
     @csrf
-    <button type="submit">Remover</button>
+    <button type="submit" class="rounded-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4">Deletar</button>
 </form>
 
 @endsection
